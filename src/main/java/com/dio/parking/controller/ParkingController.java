3bc31e02102f2d5b2616b5087a84d552126bebe3
diegorwc <wille.diegoricardo@gmail.com>
2022-11-02 10:56:50 -0300

    Swagger adicionado

--- src/main/java/com/dio/parking/controller/ParkingController.java
+++ src/main/java/com/dio/parking/controller/ParkingController.java
@@ -3,17 +3,27 @@ package com.dio.parking.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dio.parking.controller.dto.ParkingCreateDTO;
 import com.dio.parking.controller.dto.ParkingDTO;
 import com.dio.parking.controller.mapper.ParkingMapper;
 import com.dio.parking.model.Parking;
 import com.dio.parking.service.ParkingService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/parking")
+@Api(tags = "Parking Controller")
 public class ParkingController {
 
 	private final ParkingService parkingService;
@@ -25,10 +35,26 @@ public class ParkingController {
 	}
 	
 	@GetMapping
-	public List<ParkingDTO> findAll() {
+	@ApiOperation("Find all parkings")
+	public ResponseEntity<List<ParkingDTO>> findAll() {
 		List<Parking> parkingList = parkingService.findAll();
 		List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
-		return result;
+		return ResponseEntity.ok(result);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<ParkingDTO> findById(@PathVariable String id) {
+		Parking parking = parkingService.findById(id);
+		ParkingDTO result = parkingMapper.toParkingDTO(parking);
+		return ResponseEntity.ok(result);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ParkingDTO> create(@RequestBody ParkingCreateDTO dto) {
+		var parkingCreate = parkingMapper.toParkingCreate(dto);		
+		var parking = parkingService.create(parkingCreate);
+		ParkingDTO result = parkingMapper.toParkingDTO(parking);
+		return ResponseEntity.status(HttpStatus.CREATED).body(result);
 	}
 
 }
--- src/main/java/com/dio/parking/service/ParkingService.java
+++ src/main/java/com/dio/parking/service/ParkingService.java
@@ -1,5 +1,6 @@
 package com.dio.parking.service;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -18,8 +19,11 @@ public class ParkingService {
 	
 	static {
 		var id = getUUID();
+		var id2 = getUUID();
 		Parking parking = new Parking(id, "ARR-2901", "SC", "MONZA", "AZUL");
+		Parking parking2 = new Parking(id2, "RTR-2391", "PB", "VECTRA", "BRANCO");
 		parkingMap.put(id, parking);
+		parkingMap.put(id2,  parking2);
 	}
 	
 	public List<Parking> findAll() {
@@ -28,5 +32,17 @@ public class ParkingService {
 	
 	private static String getUUID() {
 		return UUID.randomUUID().toString().replace("-","");
+	}
+
+	public Parking findById(String id) {
+		return parkingMap.get(id);
+	}
+
+	public Parking create(Parking parkingCreate) {
+		String uuid = getUUID();
+		parkingCreate.setId(uuid);
+		parkingCreate.setEntryDate(LocalDateTime.now());
+		parkingMap.put(uuid, parkingCreate);
+		return parkingCreate;
 	}
 }
